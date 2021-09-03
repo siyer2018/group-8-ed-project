2b50dad0e79ebeb00ee9cfee053ea2f40b1dd99a
Adrian <101zoomzoom@gmail.com>
2021-09-03 16:39:54 -0400

    Updated processing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
 import boto3
 import pandas as pd
-import os
 from botocore.exceptions import NoCredentialsError
+from pandas.core.frame import DataFrame
+from io import StringIO
 
 CSV_HEADER: list[str] = ['DATA','EXPERIENCE']
 KEY_FILE: str = 'keys.txt'
@@ -11,65 +11,43 @@ EXP_NAME: str = 'ball_go_zoom'
 EXP_TYPE: str = '2D' #TYPE = 'AR' or '2D' or 'RL'
 OFFICIAL_BUCKET: str = 'ed1-eye-tracker'
 TEST_BUCKET: str = 'apuentebuckettest'
-PROCESS_CSV: str = "In_Process_Data.csv"
+PROCESS_CSV: str = "Process_Data.csv"
 
-def findCSV()->str:
-    """
-    Returns string representing path leading to first CSV file detected
-    """
-    for file in os.listdir(os.getcwd()):
-        if file.endswith('.csv'):
-            return os.path.join(os.getcwd(),file),file
-    print("No CSV file found.")
-    return None,'data.csv'
-CSV_PATH, CSV_NAME = findCSV()
-
-def createFakeCSV()->None:
-    """
-    Create CSV file or replace existing csv
-    Use for generating a fake csv file
-    """
-    def generateFakeData(lengthOfList:int=EXP_DURATION)->list[int]:
-        """
-        Returns list of length=lengthOfList with random ints (x,y) at each index
-        """
-        fakeData:list[int]=[]
-        for index in range(lengthOfList):
-            fakeData.append((random.randint(0,200),random.randint(0,200)))
-        return fakeData
-    if CSV_PATH is not None: os.remove(CSV_PATH)
-    data = {
-        'DATA':generateFakeData(),
-        'EXPERIENCE':[EXP_NAME]*EXP_DURATION
-    }
-    pd.DataFrame(data).to_csv(CSV_NAME,index=False)
-    return
+def stripAndCombine(frame:DataFrame, columnName:str,headers:list[str])->DataFrame:
+    for header in headers:
+        frame[header]=frame[header].str.replace('(','', regex=False)
+        frame[header]=frame[header].str.replace(')','', regex=False)
+        frame[header]=frame[header].str.replace(' ','', regex=False)
+    frame[columnName]=frame[headers].agg("'".join,axis=1)
+    frame=frame.drop(headers, axis=1)
+    return frame
 
 def readData()->None:
     """
     Read data from Unity CSV, process, and store
     """
-    df = pd.read_csv(CSV_PATH)
-    df=df[['Index','Fixation Point X','Fixation Point Y']]
-    df['EXP_TYPE']='2D'
-    df1=pd.DataFrame(df).copy()
-    df1['EXP_TYPE']='AR'
-    df=pd.concat([df,df1])
-    print(df)
-    pd.DataFrame(df).to_csv(PROCESS_CSV,index=False)
+    df = pd.read_csv("Raw_Eye_Data.csv", dtype=str)
+    exp_date=df["Date and Time"][0][:10]
+    df=df.drop("Date and Time", axis=1)
+    df=stripAndCombine(df, "Fixation Point", ["Fixation Point X", "Fixation Point Y", "Fixation Point Z", "Confidence"])
+    df=stripAndCombine(df, "Left Forward Gaze", ["Left Foward Gaze X", "Left Foward Gaze Y", "Left Foward Gaze Z"])
+    df=stripAndCombine(df, "Right Forward Gaze", ["Right Forward Gaze X", "Right Forward Gaze Y", "Right Forward Gaze Z"])
+    df=stripAndCombine(df, "Left Center Gaze", ["Left Center X", "Left Center Y", "Left Center Z", "Left Center Confidence"])
+    df=stripAndCombine(df, "Right Center Gaze", ["Right Center X", "Right Center Y", "Right Center Z", "Right Center Confidence"])
+    df=stripAndCombine(df, "Left Gaze WXYZ", ["Left Gaze W", "Left Gaze X", "Left Gaze Y", "Left Gaze Z"])
+    df=stripAndCombine(df, "Right Gaze WXYZ", ["Right Gaze W", "Right Gaze X", "Right Gaze Y", "Right Gaze Z"])
+    df=stripAndCombine(df, "Left + Right Blink", ["Left Blink", "Right Blink"])
+    pd.DataFrame(df).to_csv("Test.csv",index=False)
+    fullData=[{"Data": df.to_csv(index=False), "Date": exp_date, "Exp Name": EXP_NAME}]
+    df=pd.DataFrame(fullData)
+    pd.DataFrame(df).to_csv(PROCESS_CSV,index=False,sep=';')
     return
 
-# def readData()->None:
-#     """
-#     Read data from Unity CSV, process, and store
-#     """
-#     df = pd.read_csv(CSV_PATH, header=None)
-#     df=df.drop(0)
-#     data = {'DATA':[df,df],
-#             'EXP_TYPE':['2D','AR']}
-#     df=pd.DataFrame(data)
-#     pd.DataFrame(df).to_csv(PROCESS_CSV,index=False)
-#     return
+def openFullCSV():
+    df=pd.read_csv(PROCESS_CSV, dtype=str)
+    TESTDATA=StringIO(df.iloc[0,0])
+    df=pd.read_csv(TESTDATA)
+    pd.DataFrame(df).to_csv("Test.csv",index=False)
 
 def upload_to_aws(local_file, bucket)->bool:
     f=open(KEY_FILE)
@@ -80,7 +58,6 @@ def upload_to_aws(local_file, bucket)->bool:
     try:
         s3.upload_file(local_file, bucket, "Processed_Eye_Data.csv")
         print("Upload Successful")
-        #os.remove(CSV_NAME)
         return True
     except FileNotFoundError:
         print("The file was not found")
@@ -91,6 +68,5 @@ def upload_to_aws(local_file, bucket)->bool:
 
 readData()
 upload_to_aws(PROCESS_CSV,TEST_BUCKET)
-
-
+#openFullCSV()
 
