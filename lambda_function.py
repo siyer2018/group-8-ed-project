392f405a03160ede0c687c45d3a130294f5e239a
adrian-puente1 <56748312+adrian-puente1@users.noreply.github.com>
2021-12-01 20:02:35 -0500

    pulls csv from aws and saves to local

--- lambda_function.py
+++ lambda_function.py
@@ -1,42 +1,36 @@
+from numpy.lib.shape_base import split
 import processorFunctions
-import json
 import urllib.parse
 import boto3
-import uuid
 
-print("DATA PROCESSING INITIALIZED")
 
 s3 = boto3.client('s3')
 OUTPUT_BUCKET = "output-data-processing"
 
-def key_split(key:str)->tuple[str,str]:
+def key_split(key:str)->dict[str]:
     """
-    Returns a tuple containing experience type and experience name
+    Returns a dict containing experience type and experience name
     """
-
-    #split key into type and name
     split_key: list[str] = key.split("_")
-    exp_type: str = split_key[0]
-    exp_name: str = split_key[-1]
-    return exp_type, exp_name
+    exp_part = {
+        "type": split_key[0],
+        "name": split_key[-1]}
+    return exp_part
 
 def lambda_handler(event, context):
-    # Get the object from the event via key
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    print("Detected object is: " + str(key))
-    #loop at event['Records'][i]
-    file_path = '/tmp/'
-    download_path = file_path + key
-    s3.download_file(bucket, key, download_path)
-
-    keyDetail: tuple[str, str] = processorFunctions.key_split(key)
-    
-    fileName: str = download_path + keyDetail[0] + "_" + "Processed" + "_" + keyDetail[1]
-    fileName = download_path + keyDetail[0] + "_" + "Fixations" + "_" + keyDetail[1]
-    processorFunctions.readData(download_path, upload_path)
-    processorFunctions.getFixations(download_path, upload_path)
-
-    s3.upload_file(upload_path, OUTPUT_BUCKET, key)
-    #clear bucket
-    return "File {} successfully placed in {} bucket".format(key, OUTPUT_BUCKET)
+    # print("Detected object is: " + str(key))
+    exp_part: dict[str] = key_split(key)
+    file_part = {
+        "Raw": key,
+        "Processed": exp_part["type"] + "_Processed_" + exp_part["name"],
+        "Fixations": exp_part["type"] + "_Fixations_" + exp_part["name"],
+        "path": "s3://",
+        "folder": "tmp/",
+        "bucket": bucket + "/",
+        "output": OUTPUT_BUCKET + "/"}
+    processorFunctions.environment(file_part)
+    #clear input bucket
+    s3.delete_object(bucket, key)
+    return "File {} successfully uploaded".format(key)
--- processorFunctions.py
+++ processorFunctions.py
@@ -1,31 +1,17 @@
 import boto3
-import json
-import urllib.parse
+import awswrangler as wr
 import pandas as pd
-from numpy import mean
-from botocore.exceptions import NoCredentialsError
+from numpy import mean, string_
 from pandas.core.frame import DataFrame
 from io import StringIO
 
 KEY_FILE: str = 'keys.txt'
-EXP_NAME: str = 'ball_go_zoom' #replace with filename detection
 EXP_TYPE: str = '2D' #TYPE = 'AR' or '2D' or 'RL' #replace with filename detection
-OFFICIAL_BUCKET: str = 'output-data-processing'
 OUTER_CSV: str = "Outer_Data.csv"
-CELL_CSV: str = "Data_Cell.csv" #replace with filename detection
-FIXATION_CSV: str = "Fixation_Points.csv" #replace with filename detection
-FIXATION_OFFSET: float = 0.2
-s3 = boto3.client('s3')
+FIXATION_OFFSET: float = 0.1 #+- fixation point averaging boundary
+FIXATION_BOUNDARY: float = 0.4 #+- boundary for fixation points
 
-def readCell(cell:str)->list[str]:
-    """
-    Returns list of integers from cell string
-    """
-    values = cell.split("'")
-    values = list(map(float, values))
-    return values
-
-def readData(download_path:str, upload_path:str=None)->None:
+def readData(df:DataFrame, file_part: dict[str])->None:
     """
     Read data from Unity CSV, process, and store
     """
@@ -37,66 +23,88 @@ def readData(download_path:str, upload_path:str=None)->None:
         frame[columnName]=frame[headers].agg("'".join,axis=1)
         frame=frame.drop(headers, axis=1)
         return frame
-    df = pd.read_csv(download_path, dtype=str)
-    exp_date=df["Date and Time"][0][:10]
-    df=df.drop("Date and Time", axis=1)
-    df = df.loc[df["Left Blink"] == False]
-    df = df.loc[df["Right Blink"] == False]
-    df = df.loc[df["Confidence"] >= 0.75]
+    # exp_date: str = df["Date and Time"][0][:10]
+    
+    df = df.drop("Date and Time", axis=1)
+    df = df.loc[df["Left Blink"].isin(["False"])]
+    df = df.loc[df["Right Blink"].isin(["False"])]
+    df = df.loc[df["Confidence"].astype("float64") >= 0.75]
+
     df=stripAndCombine(df, "Fixation Point", ["Fixation Point X", "Fixation Point Y", "Fixation Point Z", "Confidence"])
-    df=stripAndCombine(df, "Left + Right Blink", ["Left Blink", "Right Blink"])
-    df=df[["Index","Fixation Point","Left + Right Blink"]]
-    pd.DataFrame(df).to_csv(CELL_CSV,index=False)
+    df=stripAndCombine(df, "Blink", ["Left Blink", "Right Blink"])
+    # new columnk IsFocused : bool
+    df=df[["Index","Fixation Point","Blink"]]
+    fixDf: DataFrame = getFixations(df)
+    pd.DataFrame(df).to_csv(file_part["Processed"],index=False,sep=',')
+    pd.DataFrame(fixDf).to_csv(file_part["Fixations"], sep=',')
     # fullData=[{"Data": df.to_csv(index=False,header=False), "Date": exp_date, "Exp Name": EXP_NAME}]
     # df=pd.DataFrame(fullData)
     # pd.DataFrame(df).to_csv(OUTER_CSV, index=False, sep=';')
     return
 
-def getFixations(fileName:str, upload_path:str=None)->None:
-    df = pd.read_csv(fileName, dtype=str)
+def getFixations(frame:DataFrame)->DataFrame:
+    def readCell(cell:str)->list[str]:
+        """
+        Returns list of integers from cell string
+        """
+        values = cell.split("'")
+        values = list(map(float, values))
+        return values
     fixDf = pd.DataFrame(columns=["Start Time","End Time","Duration","Fix Avg X","Fix Avg Y"])
-    startTime=None
+    startTime: int = None
+    fixated: int
     xList:list[float]=[]
     yList:list[float]=[]
-    for row in df.iterrows():
+    for row in frame.iterrows():
         fixPoints = readCell(row[1]["Fixation Point"])
         x = fixPoints[0]
         y = fixPoints[1]
         xList.append(x)
         yList.append(y)
-        if row[1].equals(df.iloc[0]): #initial row
+        if row[1].equals(frame.iloc[0]): #initial row
             startTime = row[1]["Index"]
-        if (x < mean(xList)-FIXATION_OFFSET) or (x > mean(xList)+FIXATION_OFFSET) or (y <mean(yList)-FIXATION_OFFSET) or (y > mean(yList)+FIXATION_OFFSET):
+        if (x < (mean(xList)-FIXATION_OFFSET)) or (x > (mean(xList)+FIXATION_OFFSET)) or (y < (mean(yList)-FIXATION_OFFSET)) or (y > (mean(yList)+FIXATION_OFFSET)):
             # append prev row
             xList.pop()
             yList.pop()
+
+            if ((mean(xList) >= 0) and (mean(yList) >= 0)):
+                fixated = 1
+            elif ((mean(xList) >= 0) and (mean(yList) <= 0)):
+                fixated = 2
+            elif ((mean(xList) <= 0) and (mean(yList) <= 0)):
+                fixated = 3
+            else:
+                fixated = 4
+            
             fixDf = fixDf.append({
                 "Start Time":startTime,
                 "End Time":row[1]["Index"],
                 "Duration":str(int(row[1]["Index"])-int(startTime)),
                 "Fix Avg X":mean(xList),
-                "Fix Avg Y":mean(yList)},ignore_index=True)
+                "Fix Avg Y":mean(yList),
+                "Fixated On Target":fixated},ignore_index=True)
             # create new row
             xList.clear()
             xList.append(x)
             yList.clear()
             yList.append(y)
             startTime = row[1]["Index"]
-        if row[1].equals(df.iloc[-1]): #last row
+        if row[1].equals(frame.iloc[-1]): #last row
             fixDf = fixDf.append({
                 "Start Time":startTime,
                 "End Time":row[1]["Index"],
                 "Duration":str(int(row[1]["Index"])-int(startTime)+1),
                 "Fix Avg X":mean(xList),
-                "Fix Avg Y":mean(yList)},ignore_index=True)
+                "Fix Avg Y":mean(yList),
+                "Fixated On Target":fixated},ignore_index=True)
             break
     #filter >1 and <-1 x and y values
-    fixDf = fixDf.loc[fixDf["Fix Avg X"] >= -1]
-    fixDf = fixDf.loc[fixDf["Fix Avg X"] <= 1]
-    fixDf = fixDf.loc[fixDf["Fix Avg Y"] >= -1]
-    fixDf = fixDf.loc[fixDf["Fix Avg Y"] <= 1]
-    pd.DataFrame(fixDf).to_csv(FIXATION_CSV, sep=',')
-    return
+    fixDf = fixDf.loc[fixDf["Fix Avg X"] >= (-1*FIXATION_BOUNDARY)]
+    fixDf = fixDf.loc[fixDf["Fix Avg X"] <= (1*FIXATION_BOUNDARY)]
+    fixDf = fixDf.loc[fixDf["Fix Avg Y"] >= (-1*FIXATION_BOUNDARY)]
+    fixDf = fixDf.loc[fixDf["Fix Avg Y"] <= (1*FIXATION_BOUNDARY)]
+    return fixDf
 
 def outer_to_data_cell():
     """
@@ -105,24 +113,54 @@ def outer_to_data_cell():
     df=pd.read_csv(OUTER_CSV, dtype=str)
     TESTDATA=StringIO(df.iloc[0,0])
     df=pd.read_csv(TESTDATA)
-    pd.DataFrame(df).to_csv(CELL_CSV,index=False)
+    # pd.DataFrame(df).to_csv(CELL_CSV,index=False)
 
-def upload_to_aws(local_file, bucket)->bool:
-    f=open(KEY_FILE)
-    ACCESS_KEY = f.readline().split('=')[1].strip()
-    SECRET_KEY = f.readline().split('=')[1].strip()
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
-    try:
-        s3.upload_file(local_file, bucket, "Processed_Eye_Data.csv")
-        print("Upload Successful")
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+def environment(file_part:dict[str])->None:
+    session = boto3.Session(
+        aws_access_key_id="xxxxxxxxxxxx",
+        aws_secret_access_key="xxxxxxxxxxx")
+    data_path: str = file_part["path"] + file_part["input"] + file_part["Raw"]
+    df: DataFrame = wr.s3.read_csv(
+        path= data_path,
+        boto3_session= session,
+        dtype = str)
+    readData(df,file_part)
+    return
 
-readData("1st_Quad.csv")
-getFixations(CELL_CSV)
+###############################
+###############################
+###############################
+###############################
+
+def key_split(key:str)->dict[str]:
+    """
+    Returns a dict containing experience type and experience name
+    """
+    split_key: list[str] = key.split("_")
+    exp_part = {
+        "type": split_key[0],
+        "name": split_key[-1]}
+    return exp_part
+
+key = "AR_Raw_AllQuadrantPerformanceTest.csv"
+bucket = "input-data-processing"
+OUTPUT_BUCKET = "output-data-processing"
+exp_part: dict[str] = key_split(key)
+file_part = {
+    "Raw": key,
+    "Processed": exp_part["type"] + "_Processed_" + exp_part["name"],
+    "Fixations": exp_part["type"] + "_Fixations_" + exp_part["name"],
+    "path": "s3://",
+    "folder": "tmp/",
+    "bucket": bucket + "/",
+    "output": OUTPUT_BUCKET + "/"}
+session = boto3.Session(
+    aws_access_key_id="AKIA443JJFV53DG4YJPH",
+    aws_secret_access_key="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")
+data_path: str = file_part["path"] + file_part["bucket"] + file_part["Raw"]
+df: DataFrame = wr.s3.read_csv(
+    path= data_path,
+    boto3_session= session,
+    dtype = str)
+
+readData(df,file_part)
